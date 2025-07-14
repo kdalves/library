2f70bf7fba321de43afab80202a0748ae0089c08
Kathleen Alves <49501299+kdalves@users.noreply.github.com>
2025-07-14 04:24:11 +0000

    Criado dashbord com todos os cadastros

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   resources :wishlists
   resources :publishers
   resources :bookcases
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   resources :books
+  root "dashboard#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
