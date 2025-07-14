565f652e98ea7e37d5951c1e6acfff178599d88c
Kathleen Alves <49501299+kdalves@users.noreply.github.com>
2025-07-14 00:48:45 +0000

    Correção de lint e novo readme

--- db/migrate/20250714002810_create_bookcases.rb
+++ db/migrate/20250714002810_create_bookcases.rb
@@ -3,7 +3,7 @@ class CreateBookcases < ActiveRecord::Migration[8.0]
     create_table :bookcases do |t|
       t.string :name
       t.integer :status
-      t.boolean :virtual      
+      t.boolean :virtual
       t.references :books, foreign_key: true
       t.references :users, foreign_key: true
 
