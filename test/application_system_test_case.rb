b28cd76c0cf1c6409f14bed8540ff9c764826885
Kathleen Alves <49501299+kdalves@users.noreply.github.com>
2025-07-14 02:27:44 +0000

    Correção para não quebrar o teste

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,19 +4,18 @@ require "capybara/minitest"
 
 if ENV["CI"]
   require "webdrivers"
-  Webdrivers::Chromedriver.required_version = '114.0.5735.90'
+  Webdrivers::Chromedriver.required_version = "114.0.5735.90"
 
   class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     driven_by :selenium, using: :headless_chrome do |options|
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1400,1400")
+      options.add_argument("--no-sandbox")
+      options.add_argument("--disable-dev-shm-usage")
+      options.add_argument("--disable-gpu")
+      options.add_argument("--window-size=1400,1400")
+    end
   end
 else
   class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-    # Skip system tests locally
     driven_by :rack_test
   end
 end
-
