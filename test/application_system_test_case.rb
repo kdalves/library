fbd0581f2e099c43a102312bc29dc7756a40e551
Kathleen Alves <49501299+kdalves@users.noreply.github.com>
2025-07-14 02:18:12 +0000

    Correção de testes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
+require "capybara/rails"
+require "capybara/minitest"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  driven_by :selenium, using: :headless_chrome do |options|
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1400,1400')
+  end
 end
