ccb1bdfa329bb769ac8b6820997958a52268cd17
Kathleen Alves <49501299+kdalves@users.noreply.github.com>
2025-07-14 03:55:56 +0000

    Ajustes no application_system_test_case e mp ci

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -9,9 +9,9 @@ end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome do |options|
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1400,1400")
+    options.add_argument "--no-sandbox"
+    options.add_argument "--disable-dev-shm-usage"
+    options.add_argument "--disable-gpu"
+    options.add_argument "--window-size=1400,1400"
   end
 end
